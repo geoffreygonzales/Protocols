cc7f2338d9825f8dfa17341ef06a805a73d6b906
Geoffrey <geoffrey@geoffreys-mbp.attlocal.net>
2017-08-04 13:27:38 -0700

    did generics

---
+++ Generics.playground/Contents.swift
@@ -0,0 +1,62 @@
+//: Playground - noun: a place where people can play
+
+import UIKit
+
+func intAdder(number : Int) -> Int {
+        return number + 1
+}
+
+intAdder(number: 15)
+
+func doubleAdder(number : Double) -> Double {
+        return number + 1.0
+}
+
+doubleAdder(number: 15.0)
+
+func genericAdder<T : Strideable>(number : T) -> T {
+        return number + 1
+}
+
+genericAdder(number: 15)
+genericAdder(number: 15.0)
+
+func intMultiplier(lhs : Int, rhs : Int) -> Int {
+        return lhs * rhs
+}
+
+intMultiplier(lhs: 2, rhs: 5)
+
+func doubleMultiplier(lhs : Double, rhs : Double) -> Double {
+        return lhs * rhs
+}
+
+doubleMultiplier(lhs: 2.0, rhs: 5.0)
+
+protocol Numeric {
+        func * (lhe : Self, rhs : Self) -> Self
+}
+
+extension Double : Numeric {}
+extension Float : Numeric {}
+extension Int : Numeric {}
+
+func genericMultiplier <T : Numeric>(lhs : T, rhs : T) -> T {
+        return lhs * rhs
+}
+
+genericMultiplier(lhs: 5, rhs: 5)
+genericMultiplier(lhs: 2.2, rhs: 5)
+
+
+
+
+
+
+
+
+
+
+
+
+
